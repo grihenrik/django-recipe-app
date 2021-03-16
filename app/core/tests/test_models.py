3e24a9a29079ff7fe8ce38dff779794ff78b84fd
Henrik Gripenberg <henrik.gripenberg@finn-id.fi>
2021-03-16 09:28:44 +0200

    added superuser user

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_succesful(self):
+        '''Test creating a new user with an email is succesful'''
+        email = 'test@g-kon.net'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        '''Test the email for a new user is normalized'''
+        email = 'test@G-KON.NET'
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        '''Test creating user with no email raises error'''
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        '''Test creating a new superuser '''
+        user = get_user_model().objects.create_superuser(
+            'test@g-kon.net',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
