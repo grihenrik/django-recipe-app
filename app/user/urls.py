aab044c86f1bbb374836ce1a52797d4c82acc3c1
Henrik Gripenberg <henrik.gripenberg@finn-id.fi>
2021-03-20 14:18:49 +0200

    create user view and token retrieval

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-
+'''Views for create and token creation'''
 from user import views
+from django.urls import path
 
 app_name = 'user'
 
 urlpatterns = [
-    path(r'^create/$', views.CreateUserView.as_view(), name='create'),
+    path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token')
 ]
